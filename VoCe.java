33bdcca3a248c086d9b06eb62c76a1139dbc896d
Hisni <hisnionline@gmail.com>
2019-10-16 20:28:03 +0530

    Main Class Modularized

--- VoCe.java
+++ VoCe.java
@@ -1,4 +1,7 @@
-import java.net.* ;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
 
 public class VoCe{
 	public static void main(String args[]){
@@ -6,17 +9,19 @@ public class VoCe{
 
             InetAddress multicastIP;
             int port;
+            int userID = getLocalAddress().hashCode();
+
             try{
                 multicastIP = InetAddress.getByName( args[0] ) ;
-                port = Integer.parseInt( "9000") ;
+                port = Integer.parseInt( "9003") ;
 
                 Audio audioObj = new Audio();
                 
-                Receiver receiver = new Receiver( multicastIP, port, audioObj );
-                Sender  sender = new Sender( multicastIP, port, audioObj );
+                Receiver receiver = new Receiver( multicastIP, port );
+                Sender  sender = new Sender( multicastIP, port, userID, audioObj );
                 Player player = new Player( audioObj );
-                sender.start();
                 receiver.start();
+                sender.start();
                 player.start();
 
             }catch(Exception e){
@@ -27,6 +32,28 @@ public class VoCe{
         else{
             System.out.println( "usage: java Control <Multicast IP>" ) ;
             return;
+		}
+    }
+    
+    public static InetAddress getLocalAddress() {
+		try{
+		    Enumeration<NetworkInterface> ifaces = NetworkInterface.getNetworkInterfaces();
+		    while( ifaces.hasMoreElements() ){
+		      	NetworkInterface iface = ifaces.nextElement();
+		     	Enumeration<InetAddress> addresses = iface.getInetAddresses();
+
+			    while( addresses.hasMoreElements() ){
+
+			        InetAddress addr = addresses.nextElement();
+			        if( addr instanceof Inet4Address && !addr.isLoopbackAddress() ){
+			          return addr;
+			        }
+		      	}
+		    }
+		    return null;
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
 		}
 	}
 }
