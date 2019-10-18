31c9008265241da985539b17cd853754c407f149
Hisni <hisnionline@gmail.com>
2019-10-18 22:18:58 +0530

    Statistics Added

--- Player.java
+++ Player.java
@@ -3,60 +3,81 @@ public class Player extends Thread {
 
     static volatile long startTime = (long)System.currentTimeMillis();
 
-    private Audio audioObj;
-    private static DataPacket Buffer[] = new DataPacket[1024];
+    private static final int BUFFSIZE = 256;
+    private static int THRESHOLD = 32;
     
-    // private int curr_sending = 0;
-    private static int threshold = 32;
+    private static DataPacket Buffer[][] = new DataPacket[2][BUFFSIZE];
     private static long currentPlaying = -1;
-    public static long unorderedPacket=0;
+    private static long unorderedPacket = 0;
+    private static long receivedPackets = 0;
+    private static long currReceivedSeq = 0;
+    private static long prevReceivedSeq = 0;
+    private long packetLoss = 0;
+    private long totalPacketLoss = 0;
+    private Audio audioObj;
+    private int ID = 0;
 
     public Player( Audio audioObj ){
         this.audioObj = audioObj;
     }
 
-    public static void addToBuffer( DataPacket dp ){
+    public static void addToBuffer( DataPacket dp, int ID ){
         if( dp.getSequenceNo() > currentPlaying ){
-            Buffer [(int)(dp.getSequenceNo()%1024)] = dp;
-        }else{
+            Buffer[ID][(int)(dp.getSequenceNo()%BUFFSIZE)] = dp;
+        }
+        currReceivedSeq = dp.getSequenceNo();
+        if( currReceivedSeq < prevReceivedSeq ){
             unorderedPacket++;
         }
-
-        if( (long)System.currentTimeMillis() > startTime + 10000 ){
-			System.out.println("Packet Size: 900 | Unorderd Packets: " + unorderedPacket );
-			startTime = System.currentTimeMillis();
-            unorderedPacket = 0;
-		}
+        prevReceivedSeq = currReceivedSeq;
+        receivedPackets++;
     }
 
     private DataPacket getAudio(){
         
         while(true){
 			int counter=0;
-			for( int i=0; i<1024; i++){
-				if( Buffer[i] != null ) counter++;
+			for( int i=0; i<BUFFSIZE; i++){
+				if( Buffer[ID][ getIndex(i) ] != null ) counter++;
 			}     
-			if( counter > threshold )break;      
+			if( counter > THRESHOLD )break;      
 	    }
         
         DataPacket dataPacket;
 
-        for(int j=0;j<1024;j++){
-            if( Buffer[j] != null){
-                long sequenceNo = Buffer[j].getSequenceNo();
+        for(int j=0;j<BUFFSIZE;j++){
+            if( Buffer[ID][ getIndex(j) ] != null){
+                long sequenceNo = Buffer[ID][ getIndex(j) ].getSequenceNo();
                 
                 if( sequenceNo >= currentPlaying ){
-                    dataPacket =  Buffer[j];
-                    Buffer[j] = null;
+                    dataPacket =  Buffer[ID][ getIndex(j) ];
+                    Buffer[ID][ getIndex(j) ] = null;
                     return dataPacket;
                 }
                 else{
-                    Buffer[j] = null;
+                    Buffer[ID][ getIndex(j) ] = null;
                 }
             }
         }
-        
         return null;
+    }
+
+    private int getIndex( int i ){
+        return (int)( ((currentPlaying%BUFFSIZE) + i + 1)%BUFFSIZE );
+    }
+
+    public long getUnordered(){
+        return unorderedPacket;
+    }
+
+    public void resetStats(){
+        unorderedPacket = 0;
+        packetLoss = 0;
+    }
+
+    public long getPacketLoss(){
+        packetLoss = (currReceivedSeq - receivedPackets) - totalPacketLoss;
+        return packetLoss;
     }
 
     public void run (){
@@ -65,6 +86,12 @@ public class Player extends Thread {
             if( dataPacket != null ){
                 currentPlaying = dataPacket.getSequenceNo();
                 audioObj.playAudio( dataPacket.getVoice() );
+            }
+
+            if( (long)System.currentTimeMillis() > startTime + 10000 ){
+                System.out.println("Packet Size: 900 | PacketLoss: " + getPacketLoss() + " | Unorderd Packets: " + getUnordered() );
+                startTime = System.currentTimeMillis();
+                resetStats();
             }
         }
     }
